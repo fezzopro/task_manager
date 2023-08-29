ba33b6a8367ee31215b45bd640cb65d3d2a0f043
Fezzopro <fezzopro@gmail.com>
2023-08-29 23:29:26 +0200

    Add root route or path to the router file

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  # Defines the root path route ("/")
+  root 'main#index'
+  get 'main/index'
   get 'tasks/index'
   get 'tasks/new'
   get 'tasks/edit'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
